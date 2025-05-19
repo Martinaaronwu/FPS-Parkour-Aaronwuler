445e9f7bc2a08522e4882aa5baefb02f5d5d3b03
Martinaaronwu <166827155+Martinaaronwu@users.noreply.github.com>
2025-05-19 09:04:18 -0300

    Initial commit

---
+++ Assets/Respawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Respawner : MonoBehaviour
+{
+    public float yRespawnValue;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < yRespawnValue)
+        {
+            Debug.Log("Me caí che");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+
+    }
+}
